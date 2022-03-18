39fc03ee6244482fcce0cb57c2f105aa12d1115c
Ethan Hearn <hearn@Ethans-MacBook-Pro.local>
2022-03-18 10:15:07 -0400

    basic functions scripted

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const add = (x, y) => {
+    return x + y;
+}
+
+const subtract = (x, y) => {
+    return x - y;
+}
+
+const multiply = (x, y) => {
+    return x * y;
+}
+
+const divide = (x, y) => {
+    return x / y;
+}
+
+const operate = (x, y, operator) => {
+    if (operator === '+') {
+        return add(x, y);
+    } else if (operator === '-') {
+        return subtract(x, y);
+    } else if (operator === '*') {
+        return multiply(x, y);
+    } else if (operator === '/') {
+        return divide(x, y);
+    }
+}
