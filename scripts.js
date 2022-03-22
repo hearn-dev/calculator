882d8d6ce404d694d9de26c78af394fff700e9cd
Ethan Hearn <hearn@hearn.nomads.utk.edu>
2022-03-22 10:06:43 -0400

    basic DOM manipulation implemented for button functionality and display updates.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,68 @@
+const calculator = () => {
+    let previous = document.querySelector("#previous");
+    let current = document.querySelector("#current");
+    let numButtons = document.querySelectorAll(".num");
+    let operators = document.querySelectorAll(".operator");
+    let clear = document.querySelector("#clear");
+    let back = document.querySelector("#back");
+    let equal = document.querySelector("#equals");
+
+    let currentOperand = "";
+    let previousOperand = "";
+    let operation = null;
+
+
+    const handleButtons = () => {
+        numButtons.forEach(btn => {
+            btn.addEventListener("click", () => {
+                console.log(btn);
+                if (btn.textContent === '.' && currentOperand.includes('.')) return;
+                else if (isNaN(currentOperand) && btn.textContent != '.') {
+                    previousOperand += ` ${currentOperand}`;
+                    currentOperand = '';
+                }
+                currentOperand += btn.textContent.toString();
+                updateDisplay();
+            })
+        })
+
+        operators.forEach(btn => {
+            btn.addEventListener("click", () => {
+                console.log(btn);
+                if (btn.textContent != '.') {
+                    previousOperand = currentOperand;
+                }
+                currentOperand = btn.textContent.toString();
+                updateDisplay();
+            })
+        })
+
+        clear.addEventListener("click", () => {
+            if (currentOperand == '') {
+                previousOperand = '';
+            } else {
+                currentOperand = '';
+            }
+            updateDisplay()
+        })
+
+        back.addEventListener("click", () => {
+            currentOperand = currentOperand.slice(0, -1);
+            updateDisplay();
+        })
+    }
+
+
+
+    const updateDisplay = () => {
+        current.textContent = currentOperand;
+        previous.textContent = previousOperand
+    }
+
+    handleButtons();
+}
+ calculator();
+
 const add = (x, y) => {
     return x + y;
 }
@@ -24,4 +89,9 @@ const operate = (x, y, operator) => {
     } else if (operator === '/') {
         return divide(x, y);
     }
+}
+
+const display = (num) => {
+    const newNum = document.createTextNode(num);
+    document.getElementById("#math").appendChild(newNum);
 }
